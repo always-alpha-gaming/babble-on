f5eb7c0ea364ffda9c4951552ebc46caf4477a15
macdja38 <jakeincanada@icloud.com>
2019-01-26 18:10:56 -0500

    Fix linting errors

--- routes/admin.js
+++ routes/admin.js
@@ -5,83 +5,85 @@
  *
  * Note: These routes only work if you have actually configured a MONGO_URI!
  * They do not work if you are using the fallback in-memory database.
- **/
-'use strict'
+ */
 
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb');
 
-let usersCollection
-if (process.env.MONGO_URI) { 
+let usersCollection;
+if (process.env.MONGO_URI) {
   // Connect to MongoDB Database and return user connection
   MongoClient.connect(process.env.MONGO_URI, (err, mongoClient) => {
-    if (err) throw new Error(err)
-    const dbName = process.env.MONGO_URI.split('/').pop().split('?').shift()
-    const db = mongoClient.db(dbName)
-    usersCollection = db.collection('users')
-  })
+    if (err) throw new Error(err);
+    const dbName = process.env.MONGO_URI.split('/')
+      .pop()
+      .split('?')
+      .shift();
+    const db = mongoClient.db(dbName);
+    usersCollection = db.collection('users');
+  });
 }
 
 module.exports = (expressApp) => {
-
   if (expressApp === null) {
-    throw new Error('expressApp option must be an express server instance')
+    throw new Error('expressApp option must be an express server instance');
   }
 
   expressApp.get('/admin/users', (req, res) => {
     // Check user is logged in and has admin access
-    if (!req.user || !req.user.admin || req.user.admin !== true)
-      return res.status('403').end()
-      
-    const page = (req.query.page && parseInt(req.query.page) > 0) ? parseInt(req.query.page) : 1
-    const sort = (req.query.sort) ? { [req.query.sort]: 1 } : {}
-    
-    let size = 10
-    if (req.query.size 
-        && parseInt(req.query.size) > 0
-        && parseInt(req.query.size) < 500) {
-      size = parseInt(req.query.size)
+    if (!req.user || !req.user.admin || req.user.admin !== true) {
+      return res.status('403')
+        .end();
     }
 
-    const skip = (size*(page-1) > 0) ? size*(page-1) : 0
-    
-    let response = {
+    const page = (req.query.page && parseInt(req.query.page, 10) > 0)
+      ? parseInt(req.query.page, 10)
+      : 1;
+    const sort = (req.query.sort) ? { [req.query.sort]: 1 } : {};
+
+    let size = 10;
+    if (req.query.size
+      && parseInt(req.query.size, 10) > 0
+      && parseInt(req.query.size, 10) < 500) {
+      size = parseInt(req.query.size, 10);
+    }
+
+    const skip = (size * (page - 1) > 0) ? size * (page - 1) : 0;
+
+    const response = {
       users: [],
-      page: page,
-      size: size,
+      page,
+      size,
       sort: req.params.sort,
-      total: 0
-    }
-    
-    if (req.params.sort) response.sort = req.params.sort
+      total: 0,
+    };
 
-    let result
-    return new Promise(function(resolve, reject) {
+    if (req.params.sort) response.sort = req.params.sort;
+
+    let result;
+    return new Promise((resolve, reject) => {
       result = usersCollection
-      .find()
-      .skip(skip)
-      .sort(sort)
-      .limit(size)
-      
+        .find()
+        .skip(skip)
+        .sort(sort)
+        .limit(size);
+
       result.toArray((err, users) => {
         if (err) {
-          reject(err)
+          reject(err);
         } else {
-          resolve(users)
+          resolve(users);
         }
+      });
+    })
+      .then((users) => {
+        response.users = users;
+        return result.count();
       })
-    })
-    .then(users => {
-      response.users = users
-      return result.count()
-    })
-    .then(count => {
-      response.total = count
-      return res.json(response)
-    })
-    .catch(err => {
-      return res.status(500).json(err)
-    })
-    
-  })
-
-}
+      .then((count) => {
+        response.total = count;
+        return res.json(response);
+      })
+      .catch(err => res.status(500)
+        .json(err));
+  });
+};
