dcb3f47c874eb2b3df10fd7b1760b80cb170ab76
Patrick Gingras <775.pg.12@gmail.com>
2019-01-26 18:08:22 -0500

    fixed more lint errors

--- models/user.js
+++ models/user.js
@@ -3,28 +3,22 @@
  * is acessible if logged in on a profile that has { "admin": true } set on it.
  * It is not used for anything else.
  */
-import fetch from 'isomorphic-fetch'
+import fetch from 'isomorphic-fetch';
 
 export default class {
-  
   static async list({
     page = 0,
-    size = 10
+    size = 10,
   } = {}) {
     return fetch(`/admin/users?page=${page}&size=${size}`, {
-      credentials: 'same-origin'
+      credentials: 'same-origin',
     })
-    .then(response => {
-      if (response.ok) {
-        return Promise.resolve(response.json())
-      } else {
-        return Promise.reject(Error('HTTP error when trying to list users'))
-      }
-    })
-    .then(data => {
-      return data
-    })
-    .catch(() => Promise.reject(Error('Error trying to list users')))
+      .then((response) => {
+        if (response.ok) {
+          return Promise.resolve(response.json());
+        }
+        return Promise.reject(Error('HTTP error when trying to list users'));
+      })
+      .catch(() => Promise.reject(Error('Error trying to list users')));
   }
-
 }
